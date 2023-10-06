e4a6eadabfc77bc5ed7109eb886ec4683b0b0923
Your Name <you@example.com>
2023-10-06 09:14:54 +0000

    [UPDATE] url balance

--- fdm_etop_connector/models/models.py
+++ fdm_etop_connector/models/models.py
@@ -13,10 +13,10 @@ class StockMove(models.Model):
     bp_old_value_qty_done = fields.Float()
     
     def action_call_balance(self):
-        response = requests.get("https://jsonplaceholder.typicode.com/todos/1",params={})
+        response = requests.get("http://192.168.1.100:7878/site/etq_hst_cab?reverse=1&limit=1",params={})
         json_response = response.json()
         self.bp_old_value_qty_done = self.qty_done
-        self.qty_done = json_response['id']
+        self.qty_done = json_response[0]['liquido']
 
     
     # def action_old_value(self):
